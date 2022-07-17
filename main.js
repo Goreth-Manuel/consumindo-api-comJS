d2ea4f41ea2c5f641ceaa7b9e86b296c18d3320d
Goreth0Manuel <gorethmanuel0@gmail.com>
2022-07-17 22:52:56 +0100

    Se aceita adiciona atualizacoes

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const url = 'http://localhost:5500/api'
 
+const newUser = {
+  name: "Goreth",
+  avatar: "http://picsum.photos/200/300",
+  city: "Luanda"
+}
+
 function getUser() {
   axios.get(url)
   .then(response => {
@@ -8,5 +14,14 @@ function getUser() {
   })
   .catch(error => console.log(error))
 }
-
 getUser()
+
+function addNewUser() {
+  axios.post(url, newUser)
+    .then(response => {
+      alert(JSON.stringify(response.data))
+    })
+    .catch(error => console.log(error))
+}
+addNewUser()
+
